5361e060457ed8eef09eedd6cf3d679b8ca43f06
nimono3 <77790085+nimono3@users.noreply.github.com>
2021-07-27 01:40:11 +0900

    Add files via upload

--- assets/dmSTG/game.js
+++ assets/dmSTG/game.js
@@ -9,25 +9,27 @@ class ballStatus {
         this.radius = radius;
         this.speed = speed;
         this.color = color;
+        this.time = 0;
     }
 
     process(id) {
-        this.x += Math.cos(this.angle) * this.speed;
-        this.y += Math.sin(this.angle) * this.speed;
-        drawCircle(this.x, this.y, this.radius, 2, "#ffffff", this.color);
+        this.x += Math.cos(this.angle) * this.speed(this.time);
+        this.y += Math.sin(this.angle) * this.speed(this.time);
+        drawCircle(this.x, this.y, this.radius, 2, "#ffffff", this.color(this.time));
         if (this.x < 0 - this.radius || this.x > mainCanvas.width + this.radius || this.y < 0 - this.radius || this.y > mainCanvas.height + this.radius) {
             ball.splice(id, 1);
         }
-        let distance = Math.pow(player.x - this.x, 2) + Math.pow(player.y - this.y, 2)
-        if (distance < 128) {
+        let distance = Math.sqrt(Math.pow(player.x - this.x, 2) + Math.pow(player.y - this.y, 2))
+        if (distance < this.radius) {
             if (!key[7] & player.life > 0) player.life--;
             ball.splice(id, 1);
-        } else if (distance < 1024) {
+        } else if (distance < 20 + this.radius) {
             score += 1;
             can2d.globalAlpha = 0.2;
             drawCircle(player.x, player.y, 16, 2, "None", "#fff");
             can2d.globalAlpha = 1;
         }
+        this.time++;
     }
 }
 class patternTemp {
@@ -43,7 +45,7 @@ class patternTemp {
 }
 class enemyTemp {
     /*敵用*/
-    constructor(hp, ballPattern, moveFunctionX, moveFunctionY, name) {
+    constructor(hp, ballPattern, moveFunctionX, moveFunctionY, ballColor, name) {
         this.x = 0;
         this.y = 0;
         this.size = player.size;
@@ -54,6 +56,7 @@ class enemyTemp {
         this.ballPattern = ballPattern;
         this.moveFunctionX = moveFunctionX;
         this.moveFunctionY = moveFunctionY;
+        this.ballColor = ballColor;
     }
 
     draw() {
@@ -68,13 +71,16 @@ class enemyTemp {
         can2d.stroke();
     }
 
-    process() {
-        let nowId = this.ballPattern(this.time);
+    process(enemyId) {
+        let nowId = this.ballPattern;
         this.x = this.moveFunctionX(this.time);
         this.y = this.moveFunctionY(this.time);
-        if (this.time % pattern[nowId].interval == 1) {
-            for (let i = 0; i < pattern[nowId].branch; i++) {
-                ball.push(new ballStatus(this.x, this.y, 2 * Math.PI * i / pattern[nowId].branch + pattern[nowId].angle(this.time, nowId), pattern[nowId].size, pattern[nowId].speed, pattern[nowId].color));
+        for (let i = 0; i < nowId.length; i++) {
+            if (this.time % pattern[nowId[i](this.time)].interval == 1) {
+                let b_angle = pattern[nowId[i](this.time)].angle(this.time, enemyId);
+                for (let j = 0; j < pattern[nowId[i](this.time)].branch; j++) {
+                    ball.push(new ballStatus(this.x, this.y, 2 * Math.PI * j / pattern[nowId[i](this.time)].branch + b_angle, pattern[nowId[i](this.time)].size, pattern[nowId[i](this.time)].speed, this.ballColor[i]));
+                }
             }
         }
         this.time++;
@@ -119,7 +125,7 @@ let player = {
     ball: []
 }
 
-/*表示文字*/
+/*タイトル文字*/
 let title = "空中楼閣";
 
 /*出る敵*/
@@ -130,47 +136,67 @@ let spawnEnemy = [
     [3, 4],
     [5],
     [8],
-    [9, 10, 11, 12]
+    [9, 10, 11, 12],
+    [14], [15], [16], [17], [18], [19], [20], [21], [22]
 ];
 let nowEnemy = spawnEnemy[nowEnemysId];   //初めの敵
 /*弾幕パターン定義*/
-/*00*/pattern.push(new patternTemp((time, id) => 0, 0, 0, 0, "#000000", 0));
-/*01*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 5, 2, "#ff0000", 10));
-/*02*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, 2, "#0000ff", 10));
-/*03*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, 2, "#00ff00", 10));
-/*04*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, 2, "#ff00ff", 10));
-/*05*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, 2, "#00ffff", 10));
-/*06*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, 2, "#a000ff", 10));
-/*07*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 8, 4, "#ff007f", 10));
-/*08*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 8, 4, "#00ffff", 10));
-/*09*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 8, 4, "#ff00ff", 10));
-/*10*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 8, 4, "#7f00ff", 10));
-/*11*/pattern.push(new patternTemp((time, id) => Math.atan2((player.y - enemy[id].y),(player.x - enemy[id].x)), 2, 20, 3, "#00ff00", 10));
+/*00*/pattern.push(new patternTemp((time, id) => 0, 0, 0, () => 0, "#000000", 0));
+/*01*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 5, () => 2, "#ff0000", 10));
+/*02*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, () => 2, "#0000ff", 10));
+/*03*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, () => 2, "#00ff00", 10));
+/*04*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, () => 2, "#ff00ff", 10));
+/*05*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, () => 2, "#00ffff", 10));
+/*06*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * ((time - 90) * -2), 1, 5, () => 2, "#a000ff", 10));
+/*07*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2) + Math.PI / 2, 1, 8, () => 4, "#ff007f", 10));
+/*08*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2) + Math.PI, 1, 8, () => 4, "#00ffff", 10));
+/*09*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2) - Math.PI / 2, 1, 8, () => 4, "#ff00ff", 10));
+/*10*/pattern.push(new patternTemp((time, id) => Math.PI / 180 * (time * 2), 1, 8, () => 4, "#7f00ff", 10));
+/*11*/pattern.push(new patternTemp((time, id) => time * time, 3, 20, () => 0.3, "#000000", 10));
+/*12*/pattern.push(new patternTemp((time, id) => Math.atan2((player.y - enemy[id].y), (player.x - enemy[id].x)), 6, 40, () => 0.8, "#000000", 10));
+/*13*/pattern.push(new patternTemp((time, id) => Math.floor(Math.random() * 3142) * 0.001, 70, 100, () => 0.5, "#000000", 10));
+/*14*/pattern.push(new patternTemp((time, id) => Math.atan2((player.y - enemy[id].y), (player.x - enemy[id].x)) + Math.floor(Math.random() * 3) - 1, 8, 10, () => 0.6, "#000000", 10));
+/*15*/pattern.push(new patternTemp((time, id) => time / 15, 6, 10, () => 0.8, "#000000", 10));
+/*16*/pattern.push(new patternTemp((time, id) => time * 60, 30, 60, () => 0.8, "#000000", 10));
+/*17*/pattern.push(new patternTemp((time, id) => Math.atan2((player.y - enemy[id].y), (player.x - enemy[id].x)), 1, 45, () => 0.8, "#000000", 20));
+/*18*/pattern.push(new patternTemp((time, id) => time * time * 4, 6, 15, () => 0.5, "#000000", 10));
+/*19*/pattern.push(new patternTemp((time, id) => time * time * 2, 6, 15, () => 0.5, "#000000", 10));
+/*patTemp定義 => 軌道,又数,間隔,速度,色彩,大きさ*/
 
 /*敵定義*/
-/*00*/enemy.push(new enemyTemp(0, ()=>0, () => 0, () => 0));
-/*01*/enemy.push(new enemyTemp(2, ()=>1, time => Math.sin(time / 100) * 400 + 400, time => 100, "01"));
-/*02*/enemy.push(new enemyTemp(2, ()=>2, time => Math.sin((time + 16) / 100) * 400 + 400, time => 100, "02"));
-/*03*/enemy.push(new enemyTemp(10, ()=>2, time => Math.abs(800 - time % 1600), time => {
+/*00*/enemy.push(new enemyTemp(0, [() => 0], () => 0, () => 0, [() => "#000"], "null"));
+/*01*/enemy.push(new enemyTemp(2, [() => 1], time => Math.sin(time / 100) * 400 + 400, time => 100, [() => "#f00"], "01"));
+/*02*/enemy.push(new enemyTemp(2, [() => 2], time => Math.sin((time + 16) / 100) * 400 + 400, time => 100, [() => "#00f"], "02"));
+/*03*/enemy.push(new enemyTemp(10, [() => 2], time => Math.abs(800 - time % 1600), time => {
     let temp = (xtime) => Math.abs(xtime, 10) + Math.abs(2 * xtime);
     return temp((Math.abs(800 - time % 1600) / 340.5) - (400 / 340.5)) * -34 + 250;
-}, "03"));
-/*04*/enemy.push(new enemyTemp(2, ()=>3, time => Math.abs(800 - (time + 800) % 1600), time => Math.sin(Math.abs(800 - (time + 800) % 1600) / 10) * -20 + 100, "04"));
-/*05*/enemy.push(new enemyTemp(20, ()=>4, time => Math.abs(800 - (time + 800) % 1600), time => Math.sin(Math.abs(800 - (time + 800) % 1600) / 10) * -20 + 100, "05"));
-/*06*/enemy.push(new enemyTemp(10, ()=>5, time => Math.abs(800 - (time + 800) % 1600), time => {
+}, [() => "#00f"], "03"));
+/*04*/enemy.push(new enemyTemp(2, [() => 3], time => Math.abs(800 - (time + 800) % 1600), time => Math.sin(Math.abs(800 - (time + 800) % 1600) / 10) * -20 + 100, [() => "#0f0"], "04"));
+/*05*/enemy.push(new enemyTemp(20, [() => 4], time => Math.abs(800 - (time + 800) % 1600), time => Math.sin(Math.abs(800 - (time + 800) % 1600) / 10) * -20 + 100, [() => "#f0f"], "05"));
+/*06*/enemy.push(new enemyTemp(10, [() => 5], time => Math.abs(800 - (time + 800) % 1600), time => {
     let temp = (xtime) => Math.abs(xtime, 10) + Math.abs(2 * xtime);
     return temp((Math.abs(800 - (time + 800) % 1600) / 340.5) - (400 / 340.5)) * -34 + 250;
-}, "06"));
-/*07*/enemy.push(new enemyTemp(2, ()=>6, time => Math.sin(time / 100) * 100 + 400, time => Math.cos(time / 100) * 100 + 100, "07"));
-/*08*/enemy.push(new enemyTemp(60, ()=>6, time => Math.abs(800 - (time + 800) % 1600), time => {
+}, [() => "#0ff"], "06"));
+/*07*/enemy.push(new enemyTemp(2, [() => 6], time => Math.sin(time / 100) * 100 + 400, time => Math.cos(time / 100) * 100 + 100, [() => "#a0f"], "07"));
+/*08*/enemy.push(new enemyTemp(60, [() => 6], time => Math.abs(800 - (time + 800) % 1600), time => {
     let temp = (xtime) => Math.abs(xtime, 10) + Math.abs(2 * xtime);
     return temp((Math.abs(800 - (time + 800) % 1600) / 340.5) - (400 / 340.5)) * -34 + 250;
-}, "08"));
-/*09*/enemy.push(new enemyTemp(4, time => 7 * (time > 300), time => 400, time => Math.sin(time / 50) * 100 + 240, "4a"));
-/*10*/enemy.push(new enemyTemp(4, time => 8 * (time > 300), time => Math.sin((time + 40) / 50) * 400 / Math.sqrt(2) + 400, time => Math.sin((time + 40) / 50) * 100 / Math.sqrt(2) + 240, "4b"));
-/*11*/enemy.push(new enemyTemp(4, time => 9 * (time > 300), time => Math.sin((time - 40) / 50) * -400 / Math.sqrt(2) + 400, time => Math.sin((time - 40) / 50) * 100 / Math.sqrt(2) + 240, "4c"));
-/*12*/enemy.push(new enemyTemp(4, time => 10 * (time > 300), time => Math.sin((time + 80) / 50) * 400 + 400, time => 240, "4d"));
-/*13*/enemy.push(new enemyTemp(10, time => 0, () => 400, time => Math.tanh(time / 100) * 240 + Math.sin(time / 50) * (Math.tanh(time / 100)) * 10, "first"));
+}, [() => "#a0f"], "08"));
+/*09*/enemy.push(new enemyTemp(4, [time => 7 * (time > 350)], time => 400, time => Math.sin(time / 50) * 200 + 240, [() => "#f07"], "4a"));
+/*10*/enemy.push(new enemyTemp(4, [time => 8 * (time > 350)], time => Math.sin((time + 40) / 50) * 200 / Math.sqrt(2) + 400, time => Math.sin((time + 40) / 50) * 200 / Math.sqrt(2) + 240, [() => "#0ff"], "4b"));
+/*11*/enemy.push(new enemyTemp(4, [time => 9 * (time > 350)], time => Math.sin((time - 40) / 50) * -200 / Math.sqrt(2) + 400, time => Math.sin((time - 40) / 50) * 200 / Math.sqrt(2) + 240, [() => "#f0f"], "4c"));
+/*12*/enemy.push(new enemyTemp(4, [time => 10 * (time > 350)], time => Math.sin((time + 80) / 50) * 200 + 400, time => 240, [() => "#70f"], "4d"));
+/*13*/enemy.push(new enemyTemp(10, [() => 0], () => 400, time => Math.tanh(time / 100) * 240 + Math.sin(time / 50) * (Math.tanh(time / 100)) * 10, [() => "#000"], "00"));
+/*14*/enemy.push(new enemyTemp(100, [time => 11 * (time > 200)], time => Math.tanh(time / 100) * 400, time => Math.tanh(time / 100) * 300, [() => "#f0f"], "guest1"));
+/*15*/enemy.push(new enemyTemp(100, [time => 12 * (time > 200)], time => 800 - Math.tanh(time / 100) * 400, time => Math.tanh(time / 100) * 300, [() => "#f70"], "guest2"));
+/*16*/enemy.push(new enemyTemp(100, [time => 13 * (time > 200)], time => Math.tanh(time / 100) * 400, () => 0, [() => "#00f"], "guest3"));
+/*17*/enemy.push(new enemyTemp(100, [time => 14 * (time > 200)], time => Math.tanh(time / 100) * 400, time => Math.tanh(time / 100) * 150, [() => "#0ff"], "guest4"));
+/*18*/enemy.push(new enemyTemp(100, [time => 15 * (time > 200)], time => Math.tanh(time / 100) * 400, () => 300, [() => "#f00"], "guest5"));
+/*19*/enemy.push(new enemyTemp(100, [time => 16 * (time > 200)], time => Math.tanh(time / 100) * 100, time => Math.tanh(time / 100) * 75, [() => "#ff0"], "guest6"));
+/*20*/enemy.push(new enemyTemp(100, [time => 17 * (time > 200)], time => 800 - Math.tanh(time / 100) * 800, time => 600 - Math.tanh(time / 100) * 600, [() => "#0f0"], "guest7"));
+/*21*/enemy.push(new enemyTemp(100, [time => 18 * (time > 200)], time => Math.tanh(time / 100) * 400, time => Math.tanh(time / 100) * 300, [() => "#7f0"], "guest8"));
+/*22*/enemy.push(new enemyTemp(100, [time => 19 * (time > 200)], time => 800 - Math.tanh(time / 100) * 400, time => 600 - Math.tanh(time / 100) * 300, [() => "#70f"], "guest9"));
+/*eneTemp => HP,弾パターン,x座標関数,y座標関数,色,名前(?)*/
 
 function start() {
     /*変数定義部*/
@@ -222,11 +248,12 @@ function drawLoop() {
         can2d.shadowBlur = 0;
         can2d.fillStyle = "#ffffff";
         can2d.font = "100px 'ＭＳ 明朝'";
-        can2d.fillText(title[0], 320, 170);
-        can2d.fillText(title[1], 420, 150);
-        can2d.fillText(title[3], 600, 180);
+        if (title.length > 4 & title.length < 8) for (let i = 0; i < title.length - 4; i++) can2d.fillText(title[i], 320 - (title.length - 4 - i) * 100, 170 + Math.sin(i) * 40);
+        if (title.length > 3) can2d.fillText(title[title.length - 4], 320, 170);
+        if (title.length > 2) can2d.fillText(title[title.length - 3], 420, 150);
+        if (title.length > 0) can2d.fillText(title[title.length - 1], 620, 180);
         can2d.fillStyle = "#ff0000";
-        can2d.fillText(title[2], 500, 200);
+        if (title.length > 1) can2d.fillText(title[title.length - 2], 520, 200);
         can2d.fillStyle = "#ffffff";
         can2d.font = "20px 'Meiryo'";
         can2d.fillText("矢印：選択　/　Z：決定", 10, 590);
@@ -248,7 +275,7 @@ function drawLoop() {
     if (mode == 1) {
         drawBack(0, "#070707");
         playerBall();
-        drawRect(player.x - player.size / 2, player.y - player.size / 2, player.size, player.size, 4, "#0000ff", "#00ffff");
+        drawRect(player.x - player.size / 2, player.y - player.size / 2, player.size - 2, player.size - 2, 6, "#0000ff", "#00ffff");
         enemysProcess();
         for (let i = 0; i < player.life; i++) drawCircle(8 + 16 * i, 8, 8, 0, "#ffefef", "#000000");
         can2d.fillStyle = "ff9090";
@@ -410,9 +437,8 @@ function playerBall() {
             player.ball.splice(i, 1);
             continue;
         }
-        for (j = 0; j < nowEnemy.length; j++) {
+        for (let j = 0; j < nowEnemy.length; j++) {
             let theEnemy = enemy[nowEnemy[j]];
-            let nowId = theEnemy.ballPattern(theEnemy.time);
             let distance = Math.pow(theEnemy.x - player.ball[i].x, 2) + Math.pow(theEnemy.y - player.ball[i].y, 2);
             if (distance < 128) {
                 theEnemy.hp--;
@@ -423,7 +449,7 @@ function playerBall() {
                     score += theEnemy.fullHp * 100;
                 }
             }
-        }
+        }//<<for j
         /*場の敵がいなくなった場合*/
         if (nowEnemy.length == 0) {
             nowEnemysId += 1;
@@ -432,16 +458,20 @@ function playerBall() {
                 /*全パターン攻略ごとにランダムに一パターン追加*/
                 var howmany = Math.floor(Math.random() * Math.floor(4));
                 spawnEnemy.push([Math.floor(Math.random() * Math.floor(enemy.length - 1)) + 1]);
-                for (var j = 0; j < howmany; j++) spawnEnemy[spawnEnemy.length - 1].push(Math.floor(Math.random() * Math.floor(enemy.length - 1)) + 1);
-            }
+                for (let j = 0; j < howmany; j++) {
+                    let in_enemy;
+                    do { in_enemy = Math.floor(Math.random() * Math.floor(enemy.length - 1)) + 1; } while (spawnEnemy[spawnEnemy.length - 1].includes(in_enemy));
+                    spawnEnemy[spawnEnemy.length - 1].push(in_enemy);
+                }
+            }//<<if (nowEnemysId == spawnEnemy.length)
             nowEnemy = spawnEnemy[nowEnemysId];
-        }
-    }
-}
+        }//<<if (nowEnemy.length == 0)
+    }//<<for i
+}//<<func playerBall
 /*enemys*/
 function enemysProcess() {
     for (let i = 0; i < nowEnemy.length; i++) {
-        enemy[nowEnemy[i]].process();
+        enemy[nowEnemy[i]].process(nowEnemy[i]);
     }
     for (let i = 0; i < ball.length; i++) {
         ball[i].process(i);
