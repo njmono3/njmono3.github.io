1a55a7aa012dd2aad8b6dab022301e6ea93fd13a
nimono3 <77790085+nimono3@users.noreply.github.com>
2022-02-08 20:09:45 +0900

    Add files via upload

--- assets/scripts/gen_note.js
+++ assets/scripts/gen_note.js
@@ -1,9 +1,19 @@
-let parser = function (string) {
-    return string.replace(/((.|\n)+)/g, "<p>$1</p>").replace(/####(.+)\n/g, "</p><h4>$1</h4><p>").replace(/###(.+)\n/g, "</p><h3>$1</h3><p>").replace(/##(.+)\n/g, "</p><h2>$1</h2><p>").replace(/#(.+)\n/g, "</p><h1>$1</h1><p>").replace(/\n/g, "<br/>")
-        .replace(/!\[(.+)\]\((.+) "(.+)"\)/g, `</p><img alt="$1" src="$2" title="$3"/><p>`).replace(/\[(.+)\]\((.+) "(.+)" "(.+)"\)/g, `<a href="$2" title="$3" target="$4">$1</a>`).replace(/\*([^*]+)\*/g, "<b>$1</b>").replace(/<p><\/p>/g, "");
+let parser = function (string) {//簡易マークダウンもどき
+    return string
+        .replace(/####(.+)/g, "</p><h4>$1</h4><p>")
+        .replace(/###(.+)/g, "</p><h3>$1</h3><p>")
+        .replace(/##(.+)/g, "</p><h2>$1</h2><p>")
+        .replace(/#(.+)/g, "</p><h1>$1</h1><p>")
+        .replace(/([^]+)/g, "<p>$1</p>")
+        .replace(/!\[(.+)\]\((.+) "(.+)"\)/g, `</p><img alt="$1" src="$2" title="$3"/><p>`)
+        .replace(/\[(.+)\]\((.+) "(.+)" "(.+)"\)/g, `<a href="$2" title="$3" target="$4">$1</a>`)
+        .replace(/\*([^*]+)\*/g, "<b>$1</b>")
+        .replace(/<p>[\s\n]*/g, "<p>")
+        .replace(/<p><\/p>/g, "")
+        .replace(/(?<!<p>)\n/g, "<br/>");
 }
 layout();
-const gen_notes = function (id = "nil", p = 0) {
+const gen_notes = function (n = "nil", p = "0") {
     const body_container = document.getElementById("body-container");
     const header = document.getElementById("header");
     const right_container = document.getElementById("right-container");
@@ -17,12 +27,13 @@ const gen_notes = function (id = "nil", p = 0) {
         if (req.readyState == 4) {
             if (req.status) {
                 if (200 <= req.status && req.status < 300 || req.status == 304) {
-                    El.appendChildren(main_contents, parser(req.responseText));
+                    let restext = req.responseText;
+                    El.appendChildren(main_contents, parser(restext));
                 }
             }
         }
     }
-    req.open("GET", "./note/" + id + ".txt");
+    req.open("GET", `./note/${n}.txt`);
     req.send()
 }
-gen_notes();
+gen_notes(new URLSearchParams(window.location.search).get("n") || "nil", new URLSearchParams(window.location.search).get("page") || "0");
