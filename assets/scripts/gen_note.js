0d582d51870fff1c6defc351380b317c87b55554
nimono3 <77790085+nimono3@users.noreply.github.com>
2022-06-06 16:57:15 +0900

    Update gen_note.js

--- assets/scripts/gen_note.js
+++ assets/scripts/gen_note.js
@@ -1,4 +1,13 @@
 layout("note");
+const gen_notes = function () {
+    const body_container = document.getElementById("body-container");
+    const header = document.getElementById("header");
+    const right_container = document.getElementById("right-container");
+    const main_container = document.getElementById("main-container");
+    const footer = document.getElementById("footer");
+    main_container.appendChild(new El("div", "", {}, { class: "main-contents note-contents", align: "left" }, [document.querySelector("#content-element")]).gen());
+}
+gen_notes();
 [...document.querySelectorAll("img")].map(
     img => 
     (
