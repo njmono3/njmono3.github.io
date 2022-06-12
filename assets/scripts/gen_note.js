93b9765ee50343a114939fc51beed117b10aa244
nimono3 <77790085+nimono3@users.noreply.github.com>
2022-06-12 22:23:53 +0900

    Update gen_note.js

--- assets/scripts/gen_note.js
+++ assets/scripts/gen_note.js
@@ -5,7 +5,15 @@ const gen_notes = function () {
     const right_container = document.getElementById("right-container");
     const main_container = document.getElementById("main-container");
     const footer = document.getElementById("footer");
-    main_container.appendChild(new El("div", "", {}, { class: "main-contents note-contents", align: "left" }, [document.querySelector("#content-element")]).gen());
+    const content = document.querySelector("#content-element");
+    const headimg = document.createElement("div");
+    headimg.style.width = "100%";
+    headimg.style.height = "300px";
+    headimg.style.background = `url("../assets/images/${content.getAttribute("head-img")}") center center / cover no-repeat`;
+    main_container.appendChild(new El("div", "", {}, { class: "main-contents note-contents", align: "left" }, [
+        headimg,
+        content
+    ]).gen());
 }
 gen_notes();
 [...document.querySelectorAll("img")].map(img => img.height = (img.alt.split("~").slice(-1)[0].match(/\d+/g) || [])[0])
