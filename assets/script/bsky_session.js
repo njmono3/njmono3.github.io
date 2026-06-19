ceca44f04dd9c495067c2cbd23cb4ea47b24e104
njmono3 <77790085+njmono3@users.noreply.github.com>
2026-06-20 02:10:36 +0900

    Add files via upload

--- assets/script/bsky_session.js
+++ assets/script/bsky_session.js
@@ -2,9 +2,19 @@ const login_button_el = document.getElementById("login-button");
 const post_button_el = document.getElementById("post-button");
 
 const client_id = encodeURIComponent("https://njmono3.github.io/oauth-client-metadata.json");
-//const request_uri = encodeURIComponent("urn:ietf:params:oauth:request_uri:req-b8d94454fd5599466b60af49f4a83dfb");
+
+const auth_metadata_endpoint = "https://bsky.social/.well-known/oauth-authorization-server";
 
 login_button_el.addEventListener("click", event => {
-    //location.href = `https://bsky.social/oauth/authorize?client_id=${client_id}&request_uri=${request_uri}`;
-    location.href = `https://bsky.social/oauth/authorize?client_id=${client_id}`;
+    fetch(auth_metadata_endpoint, "GET")
+        .then(res => res.json())
+        .then(auth_metadata_json => {
+            const auth_req_endpoint = auth_metadata_json.pushed_authorization_request_endpoint;
+            fetch(auth_req_endpoint + "?" + "did=did:plc:tpkejgkmpl7xz322emd5lwy2", "POST")
+                .then(res => res.json())
+                .then(auth_req_json => {
+                    const request_uri = auth_req_json.request_uri;
+                    location.href = `https://bsky.social/oauth/authorize?client_id=${client_id}&request_uri=${request_uri}`;
+                });
+        });
 }, false);
